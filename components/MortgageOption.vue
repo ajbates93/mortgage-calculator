d8314f1b4351ac59fecb98c3674fb042eea4f950
Alex Bates <ajbates93@gmail.com>
2024-03-13 21:15:29 +0000

    fix: formatting on inputs

--- components/MortgageOption.vue
+++ components/MortgageOption.vue
@@ -2,25 +2,25 @@
   <div>
     <h3 class="font-bold mb-5 text-2xl text-green-600">{{ title }}</h3>
     <!-- Interest Rate -->
-    <div class="grid md:grid-cols-1 grid-cols-2 items-center">
+    <div class="grid md:grid-cols-1 sm:grid-cols-2 grid-cols-1 items-center">
       <div>
         <label class="text-gray-600 dark:text-gray-300 inline-block mb-1" for="interestRate">Interest Rate (%)</label>
-        <input v-model="mortgage.interestRate" id="interestRate" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30" />
+        <input v-model="mortgage.interestRate" id="interestRate" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30 sm:w-auto w-full" />
       </div>
       <!-- Additional Fees -->
       <div>
         <label class="text-gray-600 dark:text-gray-300 inline-block mb-1" for="additionalFees">Additional Fees (£)</label>
-        <input v-model="mortgage.additionalFees" id="additionalFees" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30" />
+        <input v-model="mortgage.additionalFees" id="additionalFees" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30 sm:w-auto w-full" />
       </div>
       <!-- Mortgage Length -->
       <div>
         <label class="text-gray-600 dark:text-gray-300 inline-block mb-1" for="mortgageLength">Mortgage Length (Years)</label>
-        <input v-model="mortgage.mortgageLength" id="mortgageLength" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30" />
+        <input v-model="mortgage.mortgageLength" id="mortgageLength" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30 sm:w-auto w-full" />
       </div>
       <!-- Monthly Payments -->
       <div>
         <label class="text-gray-600 dark:text-gray-300 inline-block mb-1" for="monthlyPayments">Monthly Payments (£)</label>
-        <input v-model="mortgage.monthlyPayments" id="monthlyPayments" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30" />
+        <input v-model="mortgage.monthlyPayments" id="monthlyPayments" type="number" class="block mb-5 p-2 rounded-lg border border-green-600 border-opacity-30 sm:w-auto w-full" />
       </div>
     </div>
   </div>
